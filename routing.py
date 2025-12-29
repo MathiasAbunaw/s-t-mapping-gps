31b5477c4ce1922d39e5ad46b600fc4a169e58c6
Arreyebot Mathias Abunaw <aabunaw2006@gmail.com>
2025-12-28 23:21:16 -0600

    Updated the function get_neighborhood to be better

--- routing.py
+++ routing.py
@@ -3,20 +3,21 @@ import json
 with open('Mst_graph.json', 'r') as file:
     data = json.load(file)
 edges  = data['edges']
-def FindNeighbors(loca):
+def get_neighborhood(building_id):
     dic = {}
     for i in edges:
-        if (i["from"] == loca) or (i["to"] == loca):
-            if i["from"] == loca:
+        if (i["from"] == building_id) or (i["to"] == building_id):
+            if i["from"] == building_id:
                 dic[i["to"]] = i['distance']
             else:
                 dic[i["from"]] = i['distance']
-    print(f'{loca} neighbors:')
-    for key, value in dic.items():      
-        print(f'- {key}, ({value}m)')
+    print(f'{building_id} neighbors:')
+    if dic:
+        for key, value in dic.items():      
+            print(f'- {key}, ({value}m)')
     else:
-        print(f'No neighbors found for {loca}')
+        print(f'No neighbors found for {building_id}')
 
 if __name__ == "__main__":
     building = input("Enter a building you would like: ").strip()
-    FindNeighbors(building)
+    get_neighborhood(building)
